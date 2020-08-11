67ac037de5258843b598c50f41be7af256dd2166
shinnosuke.uesaka <shinnosuke.uesaka.sub@gmail.com>
2020-08-11 03:49:38 +0000

    bug fix

--- db/fixtures/development/kanji_csv.rb
+++ db/fixtures/development/kanji_csv.rb
@@ -3,10 +3,6 @@ require 'csv'
 
 
 CSV.foreach('db/fixtures/kanji.csv', encoding: "Shift_jis:UTF-8", headers: true) do |row|
-# row -> ["level", "category", "ex_top", "kanji", "ex_bottom", "yomi"]
-# row -> ["two", "kaki", "自然豊かで", "酪農", "が盛んだ。", "らくのう"]
-# row[0] -> "two"
-# row[1] -> "kaki"
 
   Question.seed do |s|
      s.category = row['category']
--- db/fixtures/production/kanji_csv.rb
+++ db/fixtures/production/kanji_csv.rb
@@ -3,17 +3,13 @@ require 'csv'
 
 
 CSV.foreach('db/fixtures/kanji.csv', encoding: "Shift_jis:UTF-8", headers: true) do |row|
-# row -> ["level", "category", "ex_top", "kanji", "ex_bottom", "yomi"]
-# row -> ["two", "kaki", "自然豊かで", "酪農", "が盛んだ。", "らくのう"]
-# row[0] -> "two"
-# row[1] -> "kaki"
 
   Question.seed do |s|
-     s.level = row['level'],
-     s.category = row['category'],
-     s.ex_top = row['ex_top'],
-     s.kanji = row['kanji'],
-     s.ex_bottom = row['ex_bottom'],
-     s.Fyomi = row['yomi']
+     s.category = row['category']
+     s.ex_top = row['ex_top']
+     s.kanji = row['kanji']
+     s.ex_bottom = row['ex_bottom']
+     s.yomi = row['yomi']
+     s.level = row['level']
   end
 end
